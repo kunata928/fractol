916297cc6175bfbe536dad44d9e172eca286d1d1
kunata928 <kunata928@gmail.com>
2020-02-28 20:55:09 +0300

    new

---
+++ cmake-build-debug/CMakeFiles/fractol.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fractol.dir/main.c.o"
+  "fractol"
+  "fractol.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fractol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
