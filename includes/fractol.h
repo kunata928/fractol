20adaec84ec56a666ff086346eac8d28dc20b851
kunata928 <kunata928@gmail.com>
2020-02-29 21:44:50 +0300

    newone

--- includes/fractol.h
+++ includes/fractol.h
@@ -14,6 +14,63 @@
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+# define WHITE				0xFFFFFF
+# define BLACK				0x000000
+# define WINE				0xAB271D
+# define ROSE				0x6D3445
+# define STRONG_ROSE		0x9D788F
 
+# define WINSIZEX			700
+# define WINSIZEY			700
+
+# define KEYBOARD			1
+# define MOUSE				2
+# define MOUSE_SCROLL_UP	4
+# define MOUSE_SCROLL_DOWN	5
+# define MOUSE_BUTTON_MID	3
+# define KEY_ESC			53
+# define KEY_CTRL_LEFT		256
+# define KEY_SHIFT_LEFT		257
+# define KEY_F				3
+# define KEY_H				4
+# define KEY_S				1
+# define KEY_Z				6
+# define KEY_X				7
+# define KEY_I				34
+# define KEY_T				17
+# define KEY_E				14
+# define KEY_R				15
+# define KEY_G				5
+# define KEY_B				11
+# define KEY_Q				12
+# define KEY_W				13
+# define KEY_SPACE			49
+# define KEY_PLUS			69
+# define KEY_MINUS			78
+# define KEY_NUM_1			83
+# define KEY_NUM_2			84
+# define KEY_NUM_3			85
+# define KEY_NUM_4			86
+# define KEY_NUM_6			88
+# define KEY_NUM_7			89
+# define KEY_NUM_8			91
+# define KEY_NUM_9			92
+# define KEY_NUM_UP			126
+# define KEY_NUM_DOWN		125
+# define KEY_NUM_LEFT		123
+# define KEY_NUM_RIGHT		124
+
+typedef struct				s_contr
+{
+	int		shift;
+	int		cntrl;
+	int		help;
+
+}							t_contr;
+
+typedef struct				s_fr
+{
+	t_contr contr;
+}							t_fr;
 
 #endif
